45e5bb104356f3959a493db51a0885048553e0d9
Rachel Rondon <rachel.sun.rondon@gmail.com>
2021-04-03 11:24:34 -0400

    updates

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import LandingPage from './landingpage';
+import Schedule from './schedule';
+import NavBar from './navbar.js';
+import Travel from './travel';
+import ThingsToDo from './thingsToDo';
+import Registry from './registry';
+import Photos from './photos';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  render() {
+    return (
+      <div className="App">
+      <NavBar />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={LandingPage} exact />
+            <Route path="/schedule" component={Schedule} exact />
+            <Route path="/travel" component={Travel} exact />
+            <Route path="/things-to-do" component={ThingsToDo} exact />
+            <Route path="/registry" component={Registry} exact />
+            <Route path="/photos" component={Photos} exact />
+          </Switch>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
